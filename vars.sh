06319bd1c2926df1d654d540d4f1d3c53373e525
Marshall Ashley <m.blakeashley@gmail.com>
2019-07-18 16:00:20 -0400

    update vars

--- vars.sh
+++ vars.sh
@@ -1,5 +1,9 @@
 export TF_VAR_org_id=0
 export TF_VAR_billing_account=01289B-1BDCDF-77DD18
-export TF_ADMIN=skilled-snow-247114
 export TF_PROJECT=gke-terraform-demo
+export TF_VAR_project_name=${TF_PROJECT}
+export TF_VAR_region=us-east4
+export TF_ADMIN=skilled-snow-247114
 export TF_VAR_creds="$(pwd)/creds/serviceaccount.json"
+export GOOGLE_APPLICATION_CREDENTIALS=${TF_CREDS}
+export GOOGLE_PROJECT=${TF_ADMIN}
