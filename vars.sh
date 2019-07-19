18befd0eda7f051f96f850f38aea5d3d3c081a45
Marshall Ashley <m.blakeashley@gmail.com>
2019-07-19 17:13:34 -0400

    var typo

--- vars.sh
+++ vars.sh
@@ -3,4 +3,4 @@
 export TF_PROJECT=skilled-snow-247114
 export TF_CREDS="$(pwd)/creds/serviceaccount.json"
 export GOOGLE_APPLICATION_CREDENTIALS=${TF_CREDS}
-export GOOGLE_PROJECT=${TF_ADMIN}
+export GOOGLE_PROJECT=${TF_PROJECT}
