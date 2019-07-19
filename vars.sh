746c3eb3a54f418e3ec7f7df787b9294d91ede0f
Marshall Ashley <mashley@mashley-mac.local>
2019-07-19 16:51:18 -0400

    remove items from vars.sh

--- vars.sh
+++ vars.sh
@@ -1,8 +1,6 @@
-export TF_VAR_org_id=0
-export TF_VAR_billing_account=01289B-1BDCDF-77DD18
-export TF_ADMIN=skilled-snow-247114
-export TF_VAR_project_name=skilled-snow-247114
-export TF_VAR_region=us-east4
-export TF_VAR_creds="$(pwd)/creds/serviceaccount.json"
+#!/bin/bash
+
+export TF_PROJECT=skilled-snow-247114
+export TF_CREDS="$(pwd)/creds/serviceaccount.json"
 export GOOGLE_APPLICATION_CREDENTIALS=${TF_CREDS}
 export GOOGLE_PROJECT=${TF_ADMIN}
